5363390b1682a61ba55df3af7c47fa898eacc1b6
Le Duc Thanh <thanhld@epicgroup.vn>
2025-12-28 19:28:45 +0700

    feat: refresh token

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 
@@ -11,6 +11,7 @@ export default function ProtectedRoute({ children }: ProtectedRouteProps) {
     const isAuthenticated = useSelector(
         (state: RootState) => state.auth.isAuthenticated
     );
+    const location = useLocation();
 
-    return isAuthenticated ? <>{children}</> : <Navigate to="/login" />;
+    return isAuthenticated ? <>{children}</> : <Navigate to="/login" state={{ from: location }} replace />;
 }
